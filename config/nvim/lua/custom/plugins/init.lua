fbdedb8078ef9ec22d7a3f2ae0691d309b04749a
Jay Robinson <jay.robinson2@sainsburys.co.uk>
2025-02-14 16:54:14 +0000

    vim update

--- config/nvim/lua/custom/plugins/init.lua
+++ config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,40 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{ "bullets-vim/bullets.vim" },
+
+	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+
+	{
+		"seblyng/roslyn.nvim",
+		ft = "cs",
+		opts = {
+			-- your configuration comes here; leave empty for default settings
+		},
+	},
+}
